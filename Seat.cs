b135e78ef8241251f3d6b394e4af6bdb8924b0d0
dhruvi1930 <dhruvihl369@gmail.com>
2023-01-06 10:26:03 -0500

    add cs and xml files

--- Seat.cs
+++ Seat.cs
@@ -9,7 +9,7 @@ namespace Assignment3
     public class Seat
     {
         // seat number and passenger name
-
+        
         public string seatNo { get; set; }
         public string passengerName { get; set; }
     }
